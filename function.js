05d9475b5d2db1d0f2446e001e3ac2b95f382d3b
eladcohen333 <77234157+eladcohen333@users.noreply.github.com>
2023-02-19 19:42:47 +0200

    Create function.js

---
+++ function.js
@@ -0,0 +1,12 @@
+﻿function calcStars() {
+    const warpperElements = document.querySelectorAll('.wrapper')
+    for (let i = 0; i < warpperElements.length; i++) {
+        const el = warpperElements[i];
+        const starElements = el.querySelectorAll('.fa-star');
+        const halfStarElemwnts = el.querySelectorAll('.fa-star-half-o');
+        let countDiv = document.createElement("SPAN");
+        countDiv.innerHTML = `${starElements.length + halfStarElemwnts.length * 0.5} stars`
+        el.appendChild(countDiv);
+    }
+}                            
+calcStars();
